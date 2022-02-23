7f9482b35739df1bcb84c75e2a40ea0eea2333a2
stevenantya <e0851459@u.nus.edu>
2022-02-23 19:36:25 +0800

    routews

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,27 +28,24 @@ const Pages = () => {
       <Routes>
         <Route path={ROUTES.index} element={<Home />} />
         <Route path="*" element={<Navigate to={ROUTES.index} replace />} />
-        <Route path="/" element={<Template />}>
-          <Route path="about-us" element={<AboutUsPage />}>
-            <Route path="our-family" element={<OurFamily />} />
-            <Route path="vision-mission" element={<VisionMission />} />
-            <Route path="our-structure" element={<OurStructure />} />
-            <Route path="management-committee" element={<ManagementComittee />} />
-          </Route>
+        <Route path={ROUTES.aboutUs} element={<AboutUsPage />} />
 
-          <Route path="computer-centres" element={<ComputerCentres />} />
-          <Route path="external-workshops" element={<ExternalWorkshops />} />
-          <Route path="sdp" element={<StudentDevelopementPlatform />} />
-          <Route path="pdpa" element={<PDPA />} />
-          <Route path="creation" element={<Creation />} />
-          <Route path="chariteach" element={<Chariteach />} />
-          <Route path="cyberia" element={<Cyberia />} />
-          <Route path="events" element={<Events />} />
-          <Route path="FAQ" element={<FAQ />} />
-          <Route path="feedback" element={<Feedback />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="training-workshops" element={<TrainingWorkshops />} />
-        </Route>
+        <Route path={ROUTES.ourFamily} element={<OurFamily />} />
+        <Route path={ROUTES.visionMission} element={<VisionMission />} />
+        <Route path={ROUTES.ourStructure} element={<OurStructure />} />
+        <Route path={ROUTES.managementComittee} element={<ManagementComittee />} />
+        <Route path="computer-centres" element={<ComputerCentres />} />
+        <Route path="external-workshops" element={<ExternalWorkshops />} />
+        <Route path="sdp" element={<StudentDevelopementPlatform />} />
+        <Route path="pdpa" element={<PDPA />} />
+        <Route path="creation" element={<Creation />} />
+        <Route path="chariteach" element={<Chariteach />} />
+        <Route path="cyberia" element={<Cyberia />} />
+        <Route path="events" element={<Events />} />
+        <Route path="FAQ" element={<FAQ />} />
+        <Route path="feedback" element={<Feedback />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="training-workshops" element={<TrainingWorkshops />} />
       </Routes>
     </BrowserRouter>
   )
