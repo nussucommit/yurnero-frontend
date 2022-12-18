e47e53abb6f9e5f72d4fb59c5de4969b0fcd6229
Eric Bryan <e0555789@u.nus.edu>
2022-12-18 18:21:36 +0800

    add nav bar

---
+++ src/pages/Header/index.tsx
@@ -0,0 +1,44 @@
+import Navbar from "pages/Navbar"
+import { useState } from "react"
+import { Container, Image, Input, Button } from '@chakra-ui/react'
+import { Search2Icon, CloseIcon } from '@chakra-ui/icons'
+
+import styles from './Header.module.css'
+
+const Header = () => {
+    const [isSearchBarOpen, setIsSearchBarOpen] = useState(false)
+    const [inputValue, setInputValue] = useState("")
+
+    return (
+        <div className={styles.headerContainer}>
+            <div className={styles.header}>
+                <Container classNames={styles.image}>
+                    <a href="/">
+                        <Image src="/public/images/logo.png"></Image>
+                    </a>
+                </Container>
+                <Navbar />
+                <div className={styles.searchBar}>
+                    {isSearchBarOpen ?
+                    (
+                        <button onClick={() => setIsSearchBarOpen(false)}><CloseIcon/></button>
+                    )
+                    :
+                    (
+                        <button onClick={() => setIsSearchBarOpen(true)}><Search2Icon/></button>
+                    )}
+                </div>
+            </div>
+            {isSearchBarOpen &&
+            <div className={styles.searchInput}>
+                <Input placeholder="What are you looking for?" value={inputValue} onChange={e => setInputValue(e.target.value)}/>
+                <Button className={styles.btn} colorScheme="yellow" onClick={(val) => {console.log(inputValue)}}>
+                    Search
+                </Button>
+            </div>
+            }
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/Navbar/NavbuttonWIthSubnav/index.tsx
@@ -0,0 +1,54 @@
+import { useNavigate } from 'react-router-dom'
+import {
+    useDisclosure,
+    useColorModeValue,
+    MenuItem,
+    Menu,
+    MenuButton,
+    MenuList,
+} from "@chakra-ui/react"
+import { ChevronDownIcon } from "@chakra-ui/icons"
+
+export type SubNavProps = {
+    title: string,
+    path: string
+}
+
+type NavbuttonWithSubnavProps = {
+    title: string,
+    path: string,
+    subNav: SubNavProps[]
+}
+
+const NavbuttonWithSubnav = ({title, path, subNav}: NavbuttonWithSubnavProps) => {
+    const navigate = useNavigate()
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <Menu isOpen={isOpen}>
+            <MenuButton
+                variant="ghost"
+                mx={2}
+                my={5}
+                py={[1, 1, 2]}
+                px={2}
+                borderRadius={5}
+                _hover={{ bg: useColorModeValue("#fac548", "#fac548") }}
+                fontWeight="bold"
+                onMouseEnter={onOpen}
+                onMouseLeave={onClose}
+                onClick={() => navigate(path)}
+            >
+                {title} {subNav.length > 0 ? <ChevronDownIcon/> : null}
+            </MenuButton>
+            {subNav.length > 0 &&
+                <MenuList onMouseEnter={onOpen} onMouseLeave={onClose} zIndex={10} >
+                    {subNav.map((info, idx) => (
+                        <MenuItem key={idx} onClick={() => navigate(info.path)} _hover={{ bg: "#fac548" }}>{info.title}</MenuItem>
+                    ))}
+                </MenuList>
+            }
+        </Menu>
+    )
+}
+
+export default NavbuttonWithSubnav
