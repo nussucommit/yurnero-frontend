17d420731b73bdb9ae148f7cfcd2200177a05a34
tryyang2001 <e0550472@u.nus.edu>
2022-12-20 12:26:38 +0800

    finish initial design for Vision&Mission page

--- src/pages/AboutUs/VisionMission/index.tsx
+++ src/pages/AboutUs/VisionMission/index.tsx
@@ -1,4 +1,5 @@
 import { setDefaultResultOrder } from 'dns'
+import 'bootstrap/dist/css/bootstrap.css'
 import styles from './VisionMission.module.css'
 import { Link } from 'react-router-dom'
 
@@ -13,18 +14,42 @@ const VisionMission = () => {
           <p>/</p>
           <Link to="/about-us/vision-mission">VISION & MISSION</Link>
         </div>
-        <h1>Our Family</h1>
+        <h1>Our Vision & Mission</h1>
       </div>
       <div className={styles.Content}>
-        <h2>Our Vision</h2>
-        <p>To prepare the NUS community for the future by equipping them with the necessary IT skills.</p>
-        <h2>Our Mission</h2>
-        <div className={styles.Mission}>
-          <ul>
-            <li>Provide top quality IT services that supports the functioning of the members of the NUS community.</li>
-            <li>Provide the NUS community with opportunities to pick up relevant IT skills.</li>
-            <li>Adapt to an ever-changing world and spearhead innovation in the IT field in the NUS community.</li>
-          </ul>
+        <div className={styles.Vision + ' row'}>
+          <div className={styles.VisionImage + ' col-md-5'}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/9003/9003282.png"
+              alt="Telescope"
+              className={styles.Telescope}
+            />
+          </div>
+          <div className={'col-md-7 ' + styles.VisionContent}>
+            <h2>Our Vision</h2>
+            <div className={styles.Underline + ' mx-auto'}></div>
+            <p>To prepare the NUS community for the future by equipping them with the necessary IT skills.</p>
+          </div>
+        </div>
+        <div className={styles.Mission + ' row'}>
+          <div className={styles.MissionContent + ' col-md-7'}>
+            <h2>Our Mission</h2>
+            <div className={styles.Underline + ' mx-auto'}></div>
+            <ul>
+              <li>
+                Provide top quality IT services that supports the functioning of the members of the NUS community.
+              </li>
+              <li>Provide the NUS community with opportunities to pick up relevant IT skills.</li>
+              <li>Adapt to an ever-changing world and spearhead innovation in the IT field in the NUS community.</li>
+            </ul>
+          </div>
+          <div className={styles.MissionImage + ' col-md-5'}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1183/1183866.png"
+              alt="Mission"
+              className={styles.Scroll}
+            />
+          </div>
         </div>
       </div>
     </div>
