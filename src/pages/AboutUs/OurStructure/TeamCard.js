47a50dfc5d1918bbb4dfdbb5d6dd535b9f5dde4a
tryyang2001 <e0550472@u.nus.edu>
2022-12-20 15:34:09 +0800

    finish design 'Our Structure' page, ensure responsiveness

---
+++ src/pages/AboutUs/OurStructure/TeamCard.js
@@ -0,0 +1,32 @@
+import { Image } from '@chakra-ui/react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import styles from './OurStructure.module.css'
+
+const TeamCard = props => {
+  const { header, img, content } = props
+  return (
+    <>
+      <div className={'container'}>
+        <div className={'row'}>
+          <div className={'col-md mx-auto'}>
+            <div className={styles.Card + ' card mt-5'}>
+              <div className={styles.CardHeader + ' card-header'}>
+                <h2 className={'clearfix'}>{header}</h2>
+              </div>
+              <div className={styles.CardBody + ' card-body'}>
+                <div>
+                  <Image src={img} alt="Home" className={styles.Picture} boxSize="400px" objectFit={'cover'} />
+                  <div>
+                    <p className={styles.CardContent}>{content}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TeamCard
