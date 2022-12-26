b495418a7dd38e5edddca3bc3be9eba276f842d6
Eric Bryan <e0555789@u.nus.edu>
2022-12-26 11:32:56 +0800

    make responsive

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,22 +1,29 @@
 import Navbar from "pages/Navbar"
 import { useState } from "react"
-import { Container, Image, Input, Button } from '@chakra-ui/react'
+import { Image, Input, Button } from '@chakra-ui/react'
+import { HamburgerIcon } from "@chakra-ui/icons"
 import { Search2Icon, CloseIcon } from '@chakra-ui/icons'
 
 import styles from './Header.module.css'
+import BurgerHeader from "pages/BurgerHeader"
 
 const Header = () => {
     const [isSearchBarOpen, setIsSearchBarOpen] = useState(false)
     const [inputValue, setInputValue] = useState("")
+    const [openBurgerMenu, setOpenBurgerMenu] = useState(false)
 
     return (
         <div className={styles.headerContainer}>
             <div className={styles.headerInnerContainer}>
                 <div className={styles.header}>
                     <a href="/" className={styles.image}>
-                        <Image src={require('./logo.png')}></Image>
+                        <Image src={require('../../images/logo.png')}></Image>
                     </a>
-                    <Navbar />
+                    <div>
+                        <div className={styles.Navbar}>
+                            <Navbar />
+                        </div>
+                    </div>
                     <div className={styles.searchBar}>
                         {isSearchBarOpen ?
                         (
@@ -27,7 +34,21 @@ const Header = () => {
                             <button onClick={() => setIsSearchBarOpen(true)}><Search2Icon/></button>
                         )}
                     </div>
+                    <div className={styles.DrawerHeader}>
+                        { openBurgerMenu ?
+                            <button onClick={() => {setOpenBurgerMenu(false)}}>
+                                <CloseIcon />
+                            </button>
+                            :
+                            <button onClick={() => {setOpenBurgerMenu(true)}}>
+                                <HamburgerIcon />
+                            </button> 
+                        }
+                    </div>
                 </div>
+                {openBurgerMenu && 
+                    <BurgerHeader inputValue={inputValue} setInputValue={setInputValue}/>
+                }
                 {isSearchBarOpen &&
                 <div className={styles.searchInput}>
                     <Input placeholder="What are you looking for?" value={inputValue} onChange={e => setInputValue(e.target.value)}/>
--- src/pages/Navbar/index.tsx
+++ src/pages/Navbar/index.tsx
@@ -1,97 +1,11 @@
-import { SubNavProps } from "./NavbuttonWIthSubnav"
 import NavbuttonWithSubnav from "./NavbuttonWIthSubnav"
-
-type NavButton = {
-    title: string,
-    path: string,
-    subNav: SubNavProps[]
-}
-
-const navsButton: NavButton[] = [
-    {
-        title: "ABOUT US",
-        path: "/about-us",
-        subNav: [
-            {
-                title: "Our Family",
-                path: "/about-us/our-family"
-            },
-            {
-                title: "Vision & Mission",
-                path: "/about-us/vision-mission"
-            },
-            {
-                title: "Our Structure",
-                path: "/about-us/our-structure"
-            },
-            {
-                title: "Management Committee",
-                path: "/about-us/management-committee"
-            }
-        ]
-    },
-    {
-        title: "SERVICES",
-        path: "/computer-centres",
-        subNav: [
-            {
-                title: "Computer Centres",
-                path: "/computer-centres"
-            },
-            {
-                title: "Training Workshops",
-                path: "/training-workshops"
-            },
-            {
-                title: "External Training Workshops",
-                path: "/external-workshops"
-            },
-            {
-                title: "Student Development Platform (SDP)",
-                path: "/student-development-platform"
-            }
-        ]
-    },
-    {
-        title: "EVENTS",
-        path: "/events",
-        subNav: [
-            {
-                title: "Cyberia",
-                path: "/events/cyberia"
-            },
-            {
-                title: "CREATION",
-                path: "/events/creation"
-            },
-            {
-                title: "CharITeach",
-                path: "/events/chariteach"
-            },
-        ]
-    },
-    {
-        title: "FAQ",
-        path: "/faq",
-        subNav: []
-    },
-    {
-        title: "FEEDBACK",
-        path: "/feedback",
-        subNav: []
-    },
-    {
-        title: "CONTACT US",
-        path: "/contact",
-        subNav: []
-    }
-]
+import { navs } from "constants/navs"
 
 const Navbar = () => {
     return(
         <div>
         {
-            navsButton.map((navButtonInfo, idx) => (
+            navs.map((navButtonInfo, idx) => (
                 <NavbuttonWithSubnav key={idx} title={navButtonInfo.title} path={navButtonInfo.path} subNav={navButtonInfo.subNav}/>
             ))
         }
