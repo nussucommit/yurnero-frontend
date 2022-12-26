2fadc9e04ee6eadb75b8b845608ec5c7d5a85d41
Ying Ting <95749518+ytingtan@users.noreply.github.com>
2022-12-26 09:27:33 +0800

    WEB-6/AboutUs

--- src/pages/AboutUs/AboutUsPage/index.tsx
+++ src/pages/AboutUs/AboutUsPage/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 
-import { Container } from '@chakra-ui/react'
+import { Container, Heading, Text } from '@chakra-ui/react'
 
 import styles from './AboutUsPage.module.css'
 
@@ -16,23 +16,44 @@ const AboutUsPage = () => {
         <h1>The Story</h1>
       </div>
       <Container className={styles.Content}>
+      <Heading 
+        fontWeight={600}
+        fontSize={{ base: 'xl', sm: '2xl', md: '4xl' }}
+        lineHeight={'110%'}
+        py={6}
+        color={'blue.900'}
+        alignContent={'center'}>
+        We provide IT services and trainings for the NUS community. 
+      </Heading>      
         <p>
           It all started in the dawn of the new millenia. As the great, incessant wave of Information Technology (IT)
           started to take over our lives, it became inevitable that education, as a vital part of civilisation, adopted
           IT.
         </p>
-        <h2>From the need, came the birth of us...</h2>
+        <Text
+          fontFamily={'heading'}
+          fontWeight={600}
+          mb={3}
+          fontSize={'2xl'}
+          color={'blue.600'}>
+          From the need, came the birth of us...
+        </Text>
         <p>
           From the adoption, came the need to have an organization to provide IT services to the NUS community. From the
-          need, came the birth of us, the NUS Students’ Union Committee for Information Technology (NUSSU commIT) .
+          need, came the birth of us, the <strong> NUS Students’ Union Committee for Information Technology (NUSSU commIT)</strong>. 
         </p>
         <p>
-          Today, apart from providing IT services at our two centres at YIH and AS8, we provide plenty of software
-          training workshops and specialised training events catered to the NUS community. Our aim is to provide the NUS
+          Today, apart from providing IT services at our <Link className={styles.Links}
+            to="/computer-centres" 
+            style={{color: 'darkblue', textDecoration: 'underline'}}>two centres at YIH and AS8</Link>, we provide plenty of <Link 
+          to="/training-workshops" 
+          style={{color: 'darkblue', textDecoration: 'underline'}}> software 
+          training workshops</Link>and <Link to="/external-workshops" style={{color: 'darkblue', textDecoration: 'underline'}}>specialised training events</Link>catered to the NUS community. Our aim is to provide the NUS
           community with any technology necessary for them to ace their studies and to equip them with the IT skills
           necessary to excel and differentiate themselves in their future careers.
         </p>
       </Container>
+      
     </div>
   )
 }
