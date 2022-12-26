60885c59739fbb85621362a9c87ed3efca36b9cf
Alicia <aliciamichellew@gmail.com>
2022-12-26 18:33:00 +0800

    Add nav in homepage

--- src/pages/Nav/index.tsx
+++ src/pages/Nav/index.tsx
@@ -1,24 +1,71 @@
 import styles from './Nav.module.css'
+import { Button, Stack, Box, Flex } from '@chakra-ui/react'
 
 const Nav = () => {
   return (
     <div className={styles.Nav}>
-      <p>Nav?</p>
-      {/* <Stack spacing={0} direction='row' align='center'>
-                <Button className={styles.btn}>
-                    <p className={styles.title}>COMPUTER CENTRES</p>
-                    <p className={styles.info}>lorem</p>
-                </Button>
-                <Button>
-                    TRAINING WORKSHOPS
-                </Button>
-                <Button>
-                    MAJOR EVENTS
-                </Button>
-                <Button>
-                    CONTACT US
-                </Button>
-            </Stack> */}
+      <div className={styles.Colordiff}>
+        <div className={styles.Section}>
+          <div className={styles.Content}>
+            <Box
+              as="button"
+              lineHeight="1.2"
+              fontWeight="semibold"
+              _hover={{ bg: '#152238' }}
+              padding="20px"
+              width="-webkit-fill-available"
+              borderRightColor="#152238"
+              borderRightWidth="1px"
+            >
+              <p>COMPUTER CENTRES</p>
+              <p className={styles.info}>Study, print, or scan</p>
+            </Box>
+            <Box
+              as="button"
+              lineHeight="1.2"
+              fontWeight="semibold"
+              _hover={{ bg: '#152238' }}
+              padding="20px"
+              width="-webkit-fill-available"
+              borderRightColor="#152238"
+              borderRightWidth="1px"
+              borderLeftColor="#152238"
+              borderLeftWidth="1px"
+            >
+              <p>TRAINING WORKSHOPS</p>
+              <p className={styles.info}>Stay relevant in the digital world</p>
+            </Box>
+            <Box
+              as="button"
+              lineHeight="1.2"
+              fontWeight="semibold"
+              _hover={{ bg: '#152238' }}
+              padding="20px"
+              width="-webkit-fill-available"
+              borderRightColor="#152238"
+              borderRightWidth="1px"
+              borderLeftColor="#152238"
+              borderLeftWidth="1px"
+            >
+              <p>MAJOR EVENTS</p>
+              <p className={styles.info}>Make a difference</p>
+            </Box>
+            <Box
+              as="button"
+              lineHeight="1.2"
+              fontWeight="semibold"
+              _hover={{ bg: '#152238' }}
+              padding="20px"
+              width="-webkit-fill-available"
+              borderLeftColor="#152238"
+              borderLeftWidth="1px"
+            >
+              <p>CONTACT US</p>
+              <p className={styles.info}>Get in touch</p>
+            </Box>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
