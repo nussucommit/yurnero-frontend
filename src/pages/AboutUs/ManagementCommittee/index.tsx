a9c266c4ef70a43257fb1962f848c4696a5f83a5
tryyang2001 <e0550472@u.nus.edu>
2023-01-01 20:30:35 +0800

    finish design for Management Committee page (with placeholders only, actual content is not ready)

--- src/pages/AboutUs/ManagementCommittee/index.tsx
+++ src/pages/AboutUs/ManagementCommittee/index.tsx
@@ -1,20 +1,37 @@
 import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import styles from './ManagementCommittee.module.css'
+import { Member } from './Member'
+import { directors, comm } from './comm'
 
 const ManagementCommittee = () => {
   return (
-    <div className={styles.ManagementCommittee}>
-      <div className={styles.TitleBar}>
-        <div className={styles.NavLinks}>
-          <Link to="/">HOME</Link>
-          <p>/</p>
-          <Link to="/about-us">ABOUT US</Link>
-          <p>/</p>
-          <Link to="/about-us/management-committee">MANAGEMENT COMMITTEE</Link>
+    <>
+      <div className={styles.ManagementCommittee}>
+        <div className={styles.TitleBar}>
+          <div className={styles.NavLinks}>
+            <Link to="/">HOME</Link>
+            <p>/</p>
+            <Link to="/about-us">ABOUT US</Link>
+            <p>/</p>
+            <Link to="/about-us/management-committee">MANAGEMENT COMMITTEE</Link>
+          </div>
+          <h1>30th Management Committee</h1>
         </div>
-        <h1>30th Management Committee</h1>
+        <div className={styles.MainBody}>
+          <div className={styles.DirectorContainer}>
+            {directors.map(dir => {
+              return <Member {...dir} />
+            })}
+          </div>
+          <div className={styles.MemberContainer + ' container'}>
+            {comm.map(member => {
+              return <Member {...member} />
+            })}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
