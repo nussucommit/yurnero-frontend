9286ea3fb2d502edd2e4fc8ae897a8015f573861
Eric Bryan <e0555789@u.nus.edu>
2023-01-02 15:07:07 +0800

    workshops pages template

--- src/pages/ExternalWorkshops/index.tsx
+++ src/pages/ExternalWorkshops/index.tsx
@@ -1,9 +1,46 @@
+import { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
 import styles from './ExternalWorkshops.module.css'
 
+type Content = {
+  title: string;
+  description: string;
+}
+
 const ExternalWorkshops = () => {
+  const [content, setContent] = useState<Content[]>([]);
+
+  useEffect(() => {
+    fetch("")
+      .then(res => res.json())
+      .then(
+        (res) => {
+          setContent(res as Content[])
+        }
+      )
+  }, [])
+
   return (
     <div className={styles.ExternalWorkshops}>
-      <h1>ExternalWorkshops</h1>
+      {/* add header here */}
+      <div className={styles.TitleBar}>
+        <div className={styles.NavLinks}>
+          <Link to="/">HOME</Link>
+          <p>/</p>
+          <Link to="/external-workshops">SERVICES</Link>
+        </div>
+        <h1>External Training Workshops</h1>
+      </div>
+      <div className={styles.Content}>
+        {
+          content.map((item, idx) => (
+            <div key={idx}>
+              <p className={styles.ContentTitle}>{item.title}</p>
+              <p className={styles.ContentDescription}>{item.description}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
--- src/pages/TrainingWorkshops/index.tsx
+++ src/pages/TrainingWorkshops/index.tsx
@@ -1,9 +1,45 @@
+import { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
 import styles from './TrainingWorkshops.module.css'
 
+type Content = {
+  title: string;
+  description: string;
+}
+
 const TrainingWorkshops = () => {
+  const [content, setContent] = useState<Content[]>([]);
+
+  useEffect(() => {
+    fetch("")
+      .then(res => res.json())
+      .then(
+        (res) => {
+          setContent(res as Content[])
+        }
+      )
+  }, [])
   return (
     <div className={styles.TrainingWorkshops}>
-      <h1>TrainingWorkshops</h1>
+      {/* add header here */}
+      <div className={styles.TitleBar}>
+        <div className={styles.NavLinks}>
+          <Link to="/">HOME</Link>
+          <p>/</p>
+          <Link to="/training-workshops">SERVICES</Link>
+        </div>
+        <h1>Training Workshops</h1>
+      </div>
+      <div className={styles.Content}>
+        {
+          content.map((item, idx) => (
+            <div key={idx}>
+              <p className={styles.ContentTitle}>{item.title}</p>
+              <p className={styles.ContentDescription}>{item.description}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
