278e17271a03a30a119b551a2d801a1cb501b32e
Eric Bryan <e0555789@u.nus.edu>
2023-01-09 20:50:19 +0800

    cyberia chariteach template pages

--- src/pages/Chariteach/index.tsx
+++ src/pages/Chariteach/index.tsx
@@ -1,9 +1,48 @@
+import { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
 import styles from './Chariteach.module.css'
 
+type Content = {
+  title: string;
+  description: string;
+}
+
 const Chariteach = () => {
+  const [content, setContent] = useState<Content[]>([]);
+
+  useEffect(() => {
+    fetch("")
+      .then(res => res.json())
+      .then(
+        (res) => {
+          setContent(res as Content[])
+        }
+      )
+  }, [])
+
   return (
     <div className={styles.Chariteach}>
-      <h1>Chariteach</h1>
+      {/* add header here */}
+      <div className={styles.TitleBar}>
+        <div className={styles.NavLinks}>
+          <Link to="/">HOME</Link>
+          <p>/</p>
+          <Link to="/events">EVENTS</Link>
+          <p>/</p>
+          <Link to="/events/chariteach">CHARITEACH</Link>
+        </div>
+        <h1>charITeach 2021</h1>
+      </div>
+      <div className={styles.Content}>
+        {
+          content.map((item, idx) => (
+            <div key={idx}>
+              <p className={styles.ContentTitle}>{item.title}</p>
+              <p className={styles.ContentDescription}>{item.description}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
--- src/pages/Cyberia/index.tsx
+++ src/pages/Cyberia/index.tsx
@@ -1,9 +1,48 @@
+import { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
 import styles from './Cyberia.module.css'
 
+type Content = {
+  title: string;
+  description: string;
+}
+
 const Cyberia = () => {
+  const [content, setContent] = useState<Content[]>([]);
+
+  useEffect(() => {
+    fetch("")
+      .then(res => res.json())
+      .then(
+        (res) => {
+          setContent(res as Content[])
+        }
+      )
+  }, [])
+
   return (
     <div className={styles.Cyberia}>
-      <h1>Cyberia</h1>
+      {/* add header here */}
+      <div className={styles.TitleBar}>
+        <div className={styles.NavLinks}>
+          <Link to="/">HOME</Link>
+          <p>/</p>
+          <Link to="/events">EVENTS</Link>
+          <p>/</p>
+          <Link to="/events/cyberia">CYBERIA CAMP</Link>
+        </div>
+        <h1>Cyberia Camp 2021</h1>
+      </div>
+      <div className={styles.Content}>
+        {
+          content.map((item, idx) => (
+            <div key={idx}>
+              <p className={styles.ContentTitle}>{item.title}</p>
+              <p className={styles.ContentDescription}>{item.description}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
