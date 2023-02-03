1d9ff17990c5918a243b32920729d98a910a7194
vjo-anne209 <77625659+vjo-anne209@users.noreply.github.com>
2023-02-03 22:21:28 +0800

    Update index.tsx

--- src/pages/StudentDevelopementPlatform/index.tsx
+++ src/pages/StudentDevelopementPlatform/index.tsx
@@ -1,8 +1,32 @@
 import styles from './StudentDevelopementPlatform.module.css'
+import { Link } from 'react-router-dom'
+import Header from "pages/Header"
+import { useFetchData } from "common/hooks/useFetchData"
+import { CircularProgress } from "@chakra-ui/progress";
+import { Routes } from 'constants/routes'
+import { ContentItem, convertContentItems } from "../../common/ContentItem"
 
 const StudentDevelopementPlatform = () => {
+  const [isLoading, content] = useFetchData(Routes.backendRoot + "/sdp")
+
   return (
     <div className={styles.StudentDevelopementPlatform}>
+    <Header />
+    <div className={styles.TitleBar}>
+      <div className={styles.NavLinks}>
+        <Link to="/">HOME</Link>
+        <p>/</p>
+        <Link to="/sdp">SERVICES</Link>
+      </div>
+      <h1>Student Development Platform (SDP)</h1>
+    </div>
+    <div className={styles.Content}>
+          {isLoading ? 
+            <CircularProgress isIndeterminate color='blue.300' style={{display: "flex", justifyContent: "center"}}/> 
+            :
+            convertContentItems(content as ContentItem[])
+          }
+    </div>
       <h1>StudentDevelopementPlatform</h1>
     </div>
   )
