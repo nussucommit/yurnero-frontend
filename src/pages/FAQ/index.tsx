1e7c1bc8a2655a032fe5c5082b3a13a26458c6d7
LouisLouis19 <andreaslouis.louis@gmail.com>
2023-02-18 10:20:28 +0800

    Finish FAQ page

--- src/pages/FAQ/index.tsx
+++ src/pages/FAQ/index.tsx
@@ -1,9 +1,75 @@
 import styles from './FAQ.module.css'
+import Header from 'pages/Header'
+import { Link } from 'react-router-dom'
+import { useFetchData } from 'common/hooks/useFetchData'
+import { Routes } from 'constants/routes'
+import { ContentItem } from '../../common/ContentItem'
+import { CircularProgress } from '@chakra-ui/progress'
+import { convertFaqItems } from 'common/FaqItem'
 
 const FAQ = () => {
+  const [isLoading, content] = useFetchData(Routes.backendRoot + '/faq')
+  console.log(content)
+  console.log(Object(content)['About CommIT'])
+
   return (
-    <div className={styles.FAQ}>
-      <h1>FAQ</h1>
+    <div className={styles.Faq}>
+      <Header />
+      <div className={styles.TitleBar}>
+        <div className={styles.NavLinks}>
+          <Link to="/">HOME</Link>
+          <p>/</p>
+          <Link to="/faq">FAQ</Link>
+        </div>
+        <h1>Frequently Asked Questions</h1>
+      </div>
+
+      <div className={styles.Content}>
+        <div className={styles.FaqContent}>
+          <h1 className={styles.faqsection}>About NUSSU CommIT</h1>
+          {isLoading ? (
+            <CircularProgress isIndeterminate color="blue.300" style={{ display: 'flex', justifyContent: 'center' }} />
+          ) : (
+            convertFaqItems(content as ContentItem[], 'About CommIT')
+          )}
+        </div>
+        <div className={styles.FaqContent}>
+          <br />
+          <h1 className={styles.faqsection}>Registration Procedure and Place</h1>
+          {isLoading ? (
+            <CircularProgress isIndeterminate color="blue.300" style={{ display: 'flex', justifyContent: 'center' }} />
+          ) : (
+            convertFaqItems(content as ContentItem[], 'Registration Procedure and Place')
+          )}
+        </div>
+        <div className={styles.FaqContent}>
+          <br />
+          <h1 className={styles.faqsection}>About the Workshops</h1>
+          {isLoading ? (
+            <CircularProgress isIndeterminate color="blue.300" style={{ display: 'flex', justifyContent: 'center' }} />
+          ) : (
+            convertFaqItems(content as ContentItem[], 'About the Workshops')
+          )}
+        </div>
+        <div className={styles.FaqContent}>
+          <br />
+          <h1 className={styles.faqsection}>Issues regarding Payment of Refundable Deposit</h1>
+          {isLoading ? (
+            <CircularProgress isIndeterminate color="blue.300" style={{ display: 'flex', justifyContent: 'center' }} />
+          ) : (
+            convertFaqItems(content as ContentItem[], 'Issues regarding Payment of Refundable Deposit')
+          )}
+        </div>
+        <div className={styles.FaqContent}>
+          <br />
+          <h1 className={styles.faqsection}>Trainers and Materials Coverage</h1>
+          {isLoading ? (
+            <CircularProgress isIndeterminate color="blue.300" style={{ display: 'flex', justifyContent: 'center' }} />
+          ) : (
+            convertFaqItems(content as ContentItem[], 'Trainers and Materials Coverage')
+          )}
+        </div>
+      </div>
     </div>
   )
 }
