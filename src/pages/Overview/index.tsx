b5c9182fb7f051f7b67a0a86765610b5fc6a59b4
LouisLouis19 <andreaslouis.louis@gmail.com>
2023-02-19 08:55:25 +0800

    Fix parsing bug in overview

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -6,55 +6,71 @@ import { useFetchData } from 'common/hooks/useFetchData'
 import { ContentItem, convertContentItems } from 'common/ContentItem'
 import { CircularProgress } from '@chakra-ui/progress'
 
-const BACKEND_API_HOME_ROUTE = Routes.backendRoot;
-const COMMIT_HOST_URL = "https://www.nussucommit.com"
-const TRAINING_WORKSHOP_ROUTE = "/home/training-workshops/"
-const ELEVATE_CREATIVITY_ROUTE = "/home/elevate-the-creativity/"
-const COMPUTER_CENTER_ROUTE = "/home/study-print-scan/"
+const BACKEND_API_HOME_ROUTE = Routes.backendRoot
+const COMMIT_HOST_URL = 'https://www.nussucommit.com'
+const TRAINING_WORKSHOP_ROUTE = '/home/training-workshops/'
+const ELEVATE_CREATIVITY_ROUTE = '/home/elevate-the-creativity/'
+const COMPUTER_CENTER_ROUTE = '/home/study-print-scan/'
 
 const Overview = () => {
   const [isLoadingTW, trainingWorkshopData] = useFetchData(BACKEND_API_HOME_ROUTE + TRAINING_WORKSHOP_ROUTE)
   const [isLoadingEC, elevateCreativityData] = useFetchData(BACKEND_API_HOME_ROUTE + ELEVATE_CREATIVITY_ROUTE)
   const [isLoadingCC, computerCenterData] = useFetchData(BACKEND_API_HOME_ROUTE + COMPUTER_CENTER_ROUTE)
-  
-  let trainingWorkshopContentDisplay;
-  let elevateCreativityContentDisplay;
-  let computerCenterContentDisplay;
+
+  let trainingWorkshopContentDisplay
+  let elevateCreativityContentDisplay
+  let computerCenterContentDisplay
 
   //I'm not 100% sure on handling this one, the backend formatting is incorrect so need to update. basically the backend currently adds and enclosing [] on top of the actual ContentItem[], but not sure
-  if((trainingWorkshopData as ContentItem[]).length !== 0){
-    trainingWorkshopContentDisplay = convertContentItems((trainingWorkshopData as ContentItem[])[0] as ContentItem[])
+  if ((trainingWorkshopData as ContentItem[]).length !== 0) {
+    trainingWorkshopContentDisplay = convertContentItems(trainingWorkshopData as ContentItem[] as ContentItem[])
   }
-  if((elevateCreativityData as ContentItem[]).length !== 0){
-    elevateCreativityContentDisplay = convertContentItems((elevateCreativityData as ContentItem[])[0] as ContentItem[])
+  if ((elevateCreativityData as ContentItem[]).length !== 0) {
+    elevateCreativityContentDisplay = convertContentItems(elevateCreativityData as ContentItem[] as ContentItem[])
   }
-  if((computerCenterData as ContentItem[]).length !== 0){
-    computerCenterContentDisplay = convertContentItems((computerCenterData as ContentItem[])[0] as ContentItem[])
+  if ((computerCenterData as ContentItem[]).length !== 0) {
+    computerCenterContentDisplay = convertContentItems(computerCenterData as ContentItem[] as ContentItem[])
   }
-  
+
   return (
     <div className={styles.Overview}>
       <div className={styles.Section}>
         <div className={styles.Content}>
           <Container>
-            <p>TRAINING WORKSHOP</p>
-            {isLoadingTW ? <CircularProgress isIndeterminate color="blue.300" style={{ display: 'flex', justifyContent: 'center' }} /> : trainingWorkshopContentDisplay}
+            <p className={styles.HomeHeader}>TRAINING WORKSHOP</p>
+            {isLoadingTW ? (
+              <CircularProgress
+                isIndeterminate
+                color="blue.300"
+                style={{ display: 'flex', justifyContent: 'center' }}
+              />
+            ) : (
+              trainingWorkshopContentDisplay
+            )}
           </Container>
         </div>
         <Container>
-          <Image src="https://www.nussucommit.com/images/homepage/workshop.jpg"></Image>
+          <Image src={require('../../images/home-trainingworkshop.jpeg')}></Image>
         </Container>
       </div>
 
       <div className={styles.Colordiff}>
         <div className={styles.Section}>
           <Container>
-            <Image src="https://www.nussucommit.com/images/homepage/events.jpg"></Image>
+            <Image src={require('../../images/home-elevatethecreativity.jpeg')}></Image>
           </Container>
           <div className={styles.Content}>
             <Container>
-              <p>ELEVATE THE CREATIVITY</p>
-              {isLoadingEC ? <CircularProgress isIndeterminate color="blue.300" style={{ display: 'flex', justifyContent: 'center' }} /> : elevateCreativityContentDisplay}
+              <p className={styles.HomeHeader}>ELEVATE THE CREATIVITY</p>
+              {isLoadingEC ? (
+                <CircularProgress
+                  isIndeterminate
+                  color="blue.300"
+                  style={{ display: 'flex', justifyContent: 'center' }}
+                />
+              ) : (
+                elevateCreativityContentDisplay
+              )}
             </Container>
           </div>
         </div>
@@ -63,12 +79,20 @@ const Overview = () => {
       <div className={styles.Section}>
         <div className={styles.Content}>
           <Container>
-            <p>STUDY, PRINT OR SCAN</p> 
-            {isLoadingCC ? <CircularProgress isIndeterminate color="blue.300" style={{ display: 'flex', justifyContent: 'center' }} /> : computerCenterContentDisplay}
+            <p className={styles.HomeHeader}>STUDY, PRINT OR SCAN</p>
+            {isLoadingCC ? (
+              <CircularProgress
+                isIndeterminate
+                color="blue.300"
+                style={{ display: 'flex', justifyContent: 'center' }}
+              />
+            ) : (
+              computerCenterContentDisplay
+            )}
           </Container>
         </div>
         <Container>
-          <Image src="https://www.nussucommit.com/images/homepage/centre.jpg"></Image>
+          <Image src={require('../../images/home-studyprintscan.jpeg')}></Image>
         </Container>
       </div>
     </div>
